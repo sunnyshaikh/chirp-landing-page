dc8b0135ec0e3257cb538ae91e512127924815dc
AltafAlam Shaikh <gitforaltaf@gmail.com>
2023-06-30 16:19:23 +0530

    navbar menu toggle: mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
+import Ham from "../Assets/HamburgerMenu.svg"
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
     <nav className="navbar">
       <a href="./" role="img" className="navbar__logo logo">Chirp.</a>
-      <ul className="navbar__menu">
+      <ul className={`${toggle ? "navbar__menu active" : "navbar__menu"}`}>
         <li className="navbar__item">
           <a href="#" className="navbar__list">Home</a>
         </li>
@@ -17,6 +20,9 @@ const Navbar = () => {
         </li>
         <Button text="Sign in with Twitter" />
       </ul>
+      <button className="navbar__toggle_menu" onClick={() => setToggle(!toggle)}>
+        <img src={Ham} alt="toggle" />
+      </button>
     </nav>
   )
 }
