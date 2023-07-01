4726aa1d2848cb2028039e037c642a341962f585
AltafAlam Shaikh <gitforaltaf@gmail.com>
2023-07-01 22:52:26 +0530

    pricing section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Features from "./components/Features"
 import Hero from "./components/Hero"
+import Pricing from "./components/Pricing"
 
 function App() {
   return (
     <main>
       <Hero />
       <Features />
+      <Pricing />
     </main>
   )
 }
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import CheckmarkIcon from "../Assets/Checkmark.svg"
+import Button from './Button'
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
+    <section className="pricing">
+      <div className="container">
+        <h2 className="section__title pricing__title">
+          One simple price plan.
+        </h2>
+        <p>
+          Start growing your Twitter aacount by analyzing your follower's pattern's
+        </p>
+        <div className="pricing__cards">
+          <div className="pricing__card">
+            <p className="duration">Monthly</p>
+            <h2>$9<small>/mo</small></h2>
+            <hr />
+            <ul className="pricing__features">
+              <li>
+                <img src={CheckmarkIcon} alt="checkmark" />
+                <span>Unlimited* Scheduled tweets and threads.</span>
+              </li>
+              <li>
+                <img src={CheckmarkIcon} alt="checkmark" />
+                <span>Schedule upto 3 weeks in advance.</span>
+              </li>
+              <li>
+                <img src={CheckmarkIcon} alt="checkmark" />
+                <span>Real time audience analytics tracking upto 5k followers.</span>
+              </li>
+            </ul>
+            <hr />
+            <Button text="Start Trial with Twitter" />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
